b661794a2d1bed63669a7a0dcf65962f5a25bbb2
Yoshimasa Fuwa <little.ducks@icloud.com>
2022-06-04 21:01:46 +0900

    auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsamplifytutorial8d9fd879": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {Amplify} from "aws-amplify"
+import{withAuthenticator} from "@aws-amplify/ui-react"
+import "@aws-amplify/ui-react/styles.css"
+import awsexports from "./aws-exports"
 
-function App() {
+Amplify.configure(awsexports)
+
+function App({signOut, user}) {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hallo React Amplify</h2>
+        {user ?(<><h3>権限あり:{user.username}</h3><button onClick={signOut}>Sign Out</button></>): (<h3>権限なし</h3>)}
       </header>
     </div>
+    </>
+
   );
 }
 
-export default App;
+export default withAuthenticator(App);
